18e9af1dbb3f97f9f40616bc03bf6df6c6ef6fda
ReinaldoRibeiro-dev <reinaldoribeiro.dev@gmail.com>
2025-04-24 19:43:00 -0300

    Versão atualizada do código (Ainda não 100% funcional)

--- src/App.java
+++ src/App.java
@@ -1,23 +1,38 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.List;
 import java.util.ArrayList;
 
-public class App {
+
+public class App{
     
     public static void main(String[]args){
-        ArrayList <String> nomeAlunos = new ArrayList<>();
+        List <String> nomeAlunos = new ArrayList<>();
+        List <Integer> notas = new ArrayList<>();
         Integer op; // Variável que armazena a opçao
         do{   
             limpaConsole();
             menu(args);
             System.out.print("Selecione uma das opções acima: ");
-            Scanner opSc = new Scanner(System.in);
-            op = opSc.nextInt();
+            try {
+                Scanner opSc = new Scanner(System.in);
+                op = opSc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Digite um número válido!");
+                try {
+                    Thread.sleep(3500);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                op = -1;
+            }
             limpaConsole();
             //Switch case que analisa a opção do usuário e chama a devida função
             switch (op) {
                 case 1:
-                    
+                        cadastrarNomes(nomeAlunos);
+                    break;
+                case 2:
+                        cadastrarNotas(notas, nomeAlunos);
                     break;
                 case 0:
                     System.out.println("Sistema finalizado");
@@ -40,9 +55,46 @@ public class App {
     }
     //Função que "limpa a tela". Procurar solução melhor posteriormente
     public static void limpaConsole() {
-        for(int x = 0; x < 25; x++) {
+        for(int x = 0; x < 50; x++) {
             System.out.println("\n");
         }
     }
-    
+
+    public static void cadastrarNomes(List <String> alunos){
+        System.out.print("Digite a quantidade de alunos a cadastrar: ");
+        Scanner qtdScanner = new Scanner(System.in);
+        int qtdAdicionar = qtdScanner.nextInt();
+
+        //Laço que serve apenas para garantir que
+        // o usuário não exceda o limite de alunos
+         
+        if (alunos.size() + qtdAdicionar <= 100) {
+            for (int x = 1; x <= qtdAdicionar; x++) {
+            System.out.print("Digite o nome do " + ( x ) + "° aluno: ");
+            Scanner nomeScanner = new Scanner(System.in);
+            alunos.add(nomeScanner.nextLine());
+            }
+        } else {
+            System.out.print("Excedeu o limite de alunos (100), tente novamente com um valor inferior");
+            try {
+                Thread.sleep(3500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        
+    }
+    // CONSERTAR ESSA FUNÇÃO. ERRO IndexOutOfBoundsException
+    public static void cadastrarNotas(List <Integer> notas, List <String> alunos){
+        while (true) {
+            int x = 1;
+            if (alunos.get(x) != null && notas.get(x) == null) {
+                System.out.print("Digite a nota de " + alunos.get(x) + ": ");
+                Scanner notaScanner = new Scanner(System.in);
+                notas.add(notaScanner.nextInt());
+                x++;
+            }
+        }
+    }
 }
